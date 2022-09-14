b8d3a3e68f51f9d879152e49cf747b1028240f11
AkuOO4 <akshayks2004@gmail.com>
2022-09-15 02:36:33 +0530

    by me

--- Test/Test.py
+++ Test/Test.py
@@ -20,13 +20,13 @@ female_namelist = []
 for names in female_names['name']:
 # processing on names acailable in data.
     first_name = str(names).strip().split(' ')[0]
-    male_namelist.append(first_name)
+    female_namelist.append(first_name)
 
 for names in male_names['name']:
 # processing on names acailable in data.
 
     first_name = str(names).strip().split(' ')[0]
-    female_namelist.append(first_name)
+    male_namelist.append(first_name)
 
 processed_male_namelist = []
 processed_female_namelist = []
@@ -73,16 +73,16 @@ for i in male_namelist:
 
 Final_Data={'ID':[],'Name':[],'Age':[],'Gender':[],'Comorbidity':[],'Appointment_Date':[]}
 
-start=date.today()-timedelta(days=20)
+start=date.today()-timedelta(days=50)
 
 for i in range (500):
     if i//2==0:
-        Final_Data['Age'].append(i+1)
+        Final_Data['ID'].append(i+1)
         Final_Data['Name'].append(male_namelist[i])
         Final_Data['Gender'].append('M')
 
     else:
-        Final_Data['Age'].append(i+1)
+        Final_Data['ID'].append(i+1)
         Final_Data['Name'].append(female_namelist[i])
         Final_Data['Gender'].append('F')
 
@@ -91,14 +91,25 @@ for i in range (500):
     else:
         Final_Data['Comorbidity'].append(False)
 
-    rand_date=start+timedelta(days=random.randrange(30))
-    Final_Data['Appointment_Date'].append(rand_date)
+    Final_Data['Age'].append(random.randrange(18,90))
 
+    if i//10!=0:
+        start+=timedelta(days=1)
+
+    # rand_date=start+timedelta(days=random.randrange(30))
     
+    Final_Data['Appointment_Date'].append(start)
 
+print(Final_Data)
+
+# for i in (5):
+#     print()
+# for i in Final_Data:
+#     print(i[1])
     
 
 df=pd.DataFrame(Final_Data)
 df.to_csv('Data.csv')
+
 # for i in range (5):
 #     print(male_names.name[i])
